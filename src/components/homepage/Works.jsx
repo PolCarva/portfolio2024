eb103f1cb80e6ee2ad24b976a1638a2c3ce0645e
PolCarva <pablocarvalhogimenez@gmail.com>
2024-01-22 20:40:19 -0300

    Refactor grid layout and add expandable tools list in Project component

--- src/components/homepage/Works.jsx
+++ src/components/homepage/Works.jsx
@@ -5,11 +5,11 @@ const Works = () => {
   return (
     <section id="proyects" className="bg-accent-400 -mt-px">
       <Heading title="Proyects" />
-      <div className="mt-10 flex flex-col px-10 place-items-center md:w-9/12 mx-auto lg:grid gap-16 gap-y-10 lg:grid-cols-12">
-        <div className="col-span-12">
+      <div className="mt-10 flex flex-col px-10 place-items-center md:w-9/12 mx-auto lg:grid gap-16 gap-y-10 grid-cols-12">
+        <div className="col-span-7">
           <Project />
         </div>
-        <div className="col-span-12">
+        <div className="col-span-5">
           <Project />
         </div>
       </div>
